9da29ecbadb222966592b6ce53f446d8228ac2b4
Terry Loslever <terrylos@terrylos.voo.be>
2021-11-08 21:19:31 +0100

    static address randomization

--- ASLR/ASLR.py
+++ ASLR/ASLR.py
@@ -6,6 +6,7 @@
 from Analyzer import Analyzer
 import sys
 from argparse import ArgumentParser
+import random
 
 def printSymTable(table):
 	for element in table:
@@ -18,23 +19,52 @@ def main(openFile,debug):
 	
 	if debug:
 		printSymTable(table)
-
+	#For now randomizes current address (. = 0x....) but doesn't swap regions
+	for element in table:
+		
+		if(element[0] == "curAdd"):
+			element[1] = '0x'+''.join('{:02X}'.format(int(element[1],16)+(8*random.randint(-500,500))))
+	if debug:
+		printSymTable(table)
+		
+	printBack(openFile,table)
+	
 	return 0
 
+def printBack(openFile,table):
+
+	#clears the file
+	openFile.seek(0)
+	openFile.truncate()
+	
+	#Writes back
+	openFile.write("SECTIONS\n{\n")
+	for element in table:
+		if element[0] == "curAdd":
+			openFile.write(". = "+element[1]+";\n")
+		else:
+			openFile.write("."+element[0]+":"+element[1]+"\n")
+	
+	openFile.write("}")
+	return 0
 
 if __name__ == '__main__':
 	#Gets back the path of the linker script to modify
 	parser = ArgumentParser(prog="Unikraft ASLR, linker script implementation")
 
 	parser.add_argument('--file_path', dest='path', default='./', help="Path leading to the file.")
+
 	parser.add_argument('--debug', dest='debug', default='False', 
 		help="Prints on the standard input debug informations.")
 
 	params , _ = parser.parse_known_args(sys.argv[1:])
 	
 	openFile = open(params.path,"r+")
-	
+
 	if(openFile == None):
 		print("Couldn't open the file, path may be wrong.")
 	else:
 		main(openFile,params.debug)
+		openFile.close()
+	
+	
--- ASLR/Analyzer.py
+++ ASLR/Analyzer.py
@@ -9,34 +9,55 @@ class Analyzer:
 	
 	def __init__(self,openFile):
 		self.file = openFile;
-		self.keywords = ["ENTRY","SECTIONS"]
+		self.keywords = re.compile("SECTIONS")
 		self.symbolTable = [];
 		self.buffer = '';
 		self.braceCounter = 0
-		self.parCounter = 0
-		
+		self.address = re.compile("\. =(.*);")
+		self.region = re.compile("\.(.*):")
+		self.skippedBraces = 0
+	
 	def __handlesLine(self,string):
+	
+		strLen = len(string)
+		changed = False
+		string = re.sub(self.keywords,'',string)
+		if(len(string) != strLen):
+			self.skippedBraces += 1
+		
+
 		for letter in string:
+
+			secSplit = re.split(self.address, self.buffer)[1:]
+			if len(secSplit) > 0:
+				self.buffer = ''
+				self.symbolTable.append(["curAdd",secSplit[0]])
 			match letter:
 				case '{':
-					self.braceCounter += 1
-				case '(':
-					self.parCounter += 1
+					if(self.skippedBraces == 0):
+						self.braceCounter += 1
+					elif len(self.buffer)  == 1:
+						letter = ''
 				case '}':
 					self.braceCounter -= 1
+					#Means that we skipped an opening brace before
+					if(self.braceCounter < 0):
+						self.skippedBraces -= 1
+						self.braceCounter += 1
 					#Flushes the section in the table
-					if(self.braceCounter == 0):
-						self.buffer += letter
-						secSplit = re.split("\.(.*)=", self.buffer)[1:]
-						self.symbolTable.append([secSplit[0],secSplit[1]])
-						self.buffer = ''
-				case ')':
-					self.parCounter += 1
+					if( self.braceCounter == 0):
+						secSplit = re.split(self.region, (self.buffer+letter))[1:]
+
+						if len(secSplit) > 0:
+							self.symbolTable.append([secSplit[0],secSplit[1]])
+							self.buffer = ''
+						
 			self.buffer += letter
 			
 	def analyze(self):
 		
-		for lines in self.file:
-			self.__handlesLine(lines)
+		lines = self.file.readlines()
+		for line in lines:
+			self.__handlesLine(line)
 		
 		return self.symbolTable.copy()
